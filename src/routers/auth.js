0f621cdabd35a68fcdcfbd72ab5ca7cd4292c656
Sergey Shulga <shul.sergio@gmail.com>
2025-05-22 23:54:51 +0300

    fix err with authRouter

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -9,13 +9,7 @@ import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
 const authRouter = Router();
 authRouter.post('/register', ctrlWrapper(registerUserController));
-authRouter.post('/login', (req, res) => {
-  ctrlWrapper(loginUserController);
-});
-authRouter.post('/logout', (req, res) => {
-  ctrlWrapper(logoutUserController);
-});
-authRouter.get('/refresh', (req, res) => {
-  ctrlWrapper(refreshUserSessionController);
-});
+authRouter.post('/login', ctrlWrapper(loginUserController));
+authRouter.post('/logout', ctrlWrapper(logoutUserController));
+authRouter.get('/refresh', ctrlWrapper(refreshUserSessionController));
 export default authRouter;
