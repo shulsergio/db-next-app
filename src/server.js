40c607fa0ab00e3c100bfdf5345f31ab0728f724
Sergey Shulga <shul.sergio@gmail.com>
2025-06-14 20:14:29 +0300

    Revert "deploy"
    This reverts commit fe464c61b981ce06a7329db958cfc1be18778528.

--- src/server.js
+++ src/server.js
@@ -2,35 +2,38 @@ import cors from 'cors';
 import express from 'express';
 import router from './routers/index.js';
 import cookieParser from 'cookie-parser';
-import { env } from './utils/env.js';
 // import pino from 'pino-http';
-
-const allowedOrigins = ['http://localhost:4000', 'http://localhost:3000'];
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-};
 
 export const setupServer = () => {
   const app = express();
   // const PORT = process.env.PORT || 3000;
-  app.use(cors(corsOptions));
+
   app.use(express.json());
   app.use(cookieParser());
-
+  app.use(
+    cors({
+      origin: [
+        'https://https://db-next-app.vercel.app', // Замени на свой домен фронтенда на Vercel
+        'http://localhost:3000', // Для локальной разработки фронтенда
+        'http://localhost:5000', // Если твой бэкенд локально запускается на 5000 (убери, когда полностью на Vercel)
+      ],
+      credentials: true, // Важно для куков и авторизационных заголовков
+    }),
+  );
+  // app.use(
+  //   pino({
+  //     transport: {
+  //       target: 'pino-pretty',
+  //     },
+  //   }),
+  // );
   app.get('/', (req, res) => {
     res.json({ message: 'all is ok' });
   });
   console.log('***** CONSOLE - SERVER - IS OK');
   app.use(router);
 
-  const port = Number(env('PORT', 3000));
-  app.listen(port, () => console.log(`Server is running on ${port}`));
+  // app.listen(PORT, () => {
+  //   console.log(`Server is running on port ${PORT}`);
+  // });
 };
