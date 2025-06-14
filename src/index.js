0af95f326aa6124ee0707a377239bb616d27879b
Sergey Shulga <shul.sergio@gmail.com>
2025-06-14 20:19:31 +0300

    Revert "add log for deploy"
    This reverts commit 8d92fd39a90d96373d7d4a713dccb25784bfe303.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import { initMongoDB } from './db/initMongoDB.js';
 import { setupServer } from './server.js';
 
 const bootstrap = async () => {
+  console.log('bootstrap called'); // Проверка вызова bootstrap
   await initMongoDB();
+  console.log('called setupServer after bootstrap'); // Проверка вызова bootstrap
   setupServer();
   console.log('setupServer IS OK');
 };
