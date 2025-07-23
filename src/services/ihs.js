8b9836db479b3d2e1eb5f52be90731cdda13f698
Sergey Shulga <shul.sergio@gmail.com>
2025-07-23 19:13:42 +0300

    Update ihs.js

--- src/services/ihs.js
+++ src/services/ihs.js
@@ -3,5 +3,5 @@ import { IhsCollection } from "../db/models/ihsdata.js";
 
 export const getIhs = async ({ storeId }) => {
     console.log('***** CONSOLE getIhs - storeId:', storeId);
-return await IhsCollection.find({ storeId });
+return await IhsCollection.find({ "ihsData.storeId": storeId });
 };
