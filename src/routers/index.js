a3d0f813e2a48a8b84306f3724ac63fb8f33709e
Sergey Shulga <shul.sergio@gmail.com>
2025-07-23 23:01:19 +0300

    Update index.js

--- src/routers/index.js
+++ src/routers/index.js
@@ -3,6 +3,7 @@ import authRouter from './auth.js';
 import plansRouter from './plans.js';
 import adminRouter from './admin.js';
 import ihsRouter from './ihs.js';
+import newdataRouter from './newdata.js';
 
 const indexRouter = Router();
 console.log('***** CONSOLE - index - IS OK');
@@ -10,5 +11,5 @@ indexRouter.use('/auth', authRouter);
 indexRouter.use('/plans', plansRouter);
 indexRouter.use('/admin', adminRouter);
 indexRouter.use('/ihsdata', ihsRouter);
-indexRouter.use('/newdata', ihsRouter);
+indexRouter.use('/newdata', newdataRouter);
 export default indexRouter;
