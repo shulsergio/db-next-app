554c1a0b9af1be01f3c57f9b124862ea651710d1
Sergey Shulga <shul.sergio@gmail.com>
2025-08-20 19:41:58 +0300

    Update focusModels.js

--- src/services/focusModels.js
+++ src/services/focusModels.js
@@ -4,15 +4,18 @@ import { focusModelsCollection } from '../db/models/focusModels.js';
 
 export const getFocusModels = async (type) => {
   console.log('**???*** CONSOLE getFocusModels - type:', type);
-  const limit = 20;
-  const page = 1;
-  const skipModels = (page - 1) * limit;
-  console.log('**???*** CONSOLE getFocusModels - limit:', limit);
-  console.log('**???*** CONSOLE getFocusModels - page:', page);
-  console.log('**???*** CONSOLE getFocusModels - skipModels:', skipModels);
-  return await focusModelsCollection
-    .find({ type })
-    .skip(skipModels)
-    .limit(limit)
-    .exec();
+
+  return await focusModelsCollection.find({ type });
 };
+
+// const limit = 20;
+// const page = 1;
+// const skipModels = (page - 1) * limit;
+// console.log('**???*** CONSOLE getFocusModels - limit:', limit);
+// console.log('**???*** CONSOLE getFocusModels - page:', page);
+// console.log('**???*** CONSOLE getFocusModels - skipModels:', skipModels);
+// return await focusModelsCollection
+//   .find({ type })
+//   .skip(skipModels)
+//   .limit(limit)
+//   .exec();
