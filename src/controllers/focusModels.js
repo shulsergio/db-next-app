243b2d9c80be952b28028f4bcbd71683e03e2d31
Sergey Shulga <shul.sergio@gmail.com>
2025-08-23 21:31:46 +0300

    add pagination for focus models

--- src/controllers/focusModels.js
+++ src/controllers/focusModels.js
@@ -3,14 +3,21 @@ import { getFocusModels } from '../services/focusModels.js';
 export const getFocusModelsController = async (req, res, next) => {
   try {
     const { type } = req.params;
+    const { curPage, limit } = req.query;
     console.log('!!!getFocusModelsController - type:', type);
+    console.log('!!!getFocusModelsController - curPage:', curPage);
+    console.log('!!!getFocusModelsController - limit:', limit);
     if (!type) {
       return res.status(400).json({
         status: 400,
         message: 'Bad Request dep.',
       });
     }
-    const data = await getFocusModels(type);
+
+    const page = parseInt(curPage, 10) || 1;
+    const size = parseInt(limit, 10) || 20;
+
+    const data = await getFocusModels(type, page, size);
     res.status(200).json({
       status: 200,
       message: 'Successfully retrieved type!',
--- src/services/focusModels.js
+++ src/services/focusModels.js
@@ -2,10 +2,17 @@
 
 import { focusModelsCollection } from '../db/models/focusModels.js';
 
-export const getFocusModels = async (type) => {
-  console.log('**???*** CONSOLE getFocusModels - type:', type);
-
-  return await focusModelsCollection.find({ type });
+export const getFocusModels = async (type, limit, page) => {
+  const skipModels = (page - 1) * limit;
+  console.log('**???*** CONSOLE getFocusModels - limit:', limit);
+  console.log('**???*** CONSOLE getFocusModels - page:', page);
+  console.log('**???*** CONSOLE getFocusModels - skipModels:', skipModels);
+  return await focusModelsCollection
+    .find({ type })
+    .skip(skipModels)
+    .limit(limit)
+    .exec();
+  // return await focusModelsCollection.find({ type });
 };
 
 // const limit = 20;
