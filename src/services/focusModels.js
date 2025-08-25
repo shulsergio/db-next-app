c489599b7737721b20f6248d851d5c88435b9f9d
Sergey Shulga <shul.sergio@gmail.com>
2025-08-26 01:34:37 +0300

    Update focusModels.js

--- src/services/focusModels.js
+++ src/services/focusModels.js
@@ -5,13 +5,14 @@ import { focusModelsCollection } from '../db/models/focusModels.js';
 export const getFocusModels = async (editType, limit, curPage, selectedPrd) => {
   try {
     const skipModels = (curPage - 1) * limit;
-    console.log('**???*** CONSOLE getFocusModels - editType:', editType);
+    const type = editType;
+    console.log('**???*** CONSOLE getFocusModels - editType:', type);
     console.log('**???*** CONSOLE getFocusModels - limit:', limit);
     console.log('**???*** CONSOLE getFocusModels - page:', curPage);
     console.log('**???*** CONSOLE getFocusModels - skipModels:', skipModels);
     console.log('**???*** CONSOLE getFocusModels - selectedPrd:', selectedPrd);
 
-    const filter = { editType };
+    const filter = { type };
 
     if (selectedPrd && selectedPrd !== 'all') {
       filter.prd = selectedPrd;
