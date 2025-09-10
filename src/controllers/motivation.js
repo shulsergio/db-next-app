edcb97a92cc94c292ed29b7dd1bea559a99c2a51
Sergey Shulga <shul.sergio@gmail.com>
2025-09-11 00:26:44 +0300

    add cluster to find

--- src/controllers/motivation.js
+++ src/controllers/motivation.js
@@ -1,8 +1,9 @@
 import { AVdavMotivationCollection } from '../db/models/davMotivation.js';
 
 export const getDavMotivationController = async (req, res, next) => {
+  const { cluster } = req.query;
   try {
-    const davMotivations = await AVdavMotivationCollection.find({});
+    const davMotivations = await AVdavMotivationCollection.find({ cluster });
     res.status(200).json({
       status: 200,
       message: 'Successfully retrieved DAV motivations!',
