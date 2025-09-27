35148e21090dc2577a60f93c52c9375e4691f87a
Sergey Shulga <shul.sergio@gmail.com>
2025-09-27 23:00:55 +0300

    change CategorySchema in bonusRowdatas

--- src/db/models/topBonuses.js
+++ src/db/models/topBonuses.js
@@ -1,6 +1,30 @@
 import { model, Schema } from 'mongoose';
 
-const CategorySchema = new Schema({
+// const CategorySchema = new Schema({
+//     account: { type: String, required: true },
+//     item: { type: String, required: true },
+//     soqty: { type: Number, required: true },
+//     bonus: { type: Number, required: true },
+//     prd: { type: String, required: true },
+//     day: { type: String, required: true },
+//     week: { type: String, required: true },
+// }, { _id: false });
+
+// const BonusDataItemSchema = new Schema({
+//     categories: { type: [CategorySchema], required: true },
+
+// }, { _id: false });
+
+// const TopBonusesSchema = new Schema({
+//     bonusData: { type: [BonusDataItemSchema], required: true },
+//         storeId: { type: String, required: true }
+// });
+
+// export const topBonusesCollection = model('topbonusdatas', TopBonusesSchema);
+
+const CategorySchema = new Schema(
+  {
+    storeId: { type: String, required: true },
     account: { type: String, required: true },
     item: { type: String, required: true },
     soqty: { type: Number, required: true },
@@ -8,16 +32,8 @@ const CategorySchema = new Schema({
     prd: { type: String, required: true },
     day: { type: String, required: true },
     week: { type: String, required: true },
-}, { _id: false });
+  },
+  { _id: false },
+);
 
-const BonusDataItemSchema = new Schema({
-    categories: { type: [CategorySchema], required: true },
-
-}, { _id: false });
-
-const TopBonusesSchema = new Schema({
-    bonusData: { type: [BonusDataItemSchema], required: true },
-        storeId: { type: String, required: true }
-});
-
-export const topBonusesCollection = model('topbonusdatas', TopBonusesSchema);
+export const topBonusesCollection = model('bonusRowdatas', CategorySchema);
