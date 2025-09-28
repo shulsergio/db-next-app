c332dbc60f119afe76c054182bd7d3428fe875b3
Sergey Shulga <shul.sergio@gmail.com>
2025-09-28 23:20:43 +0300

    fix err on model of DavMotivation

--- src/db/models/davMotivation.js
+++ src/db/models/davMotivation.js
@@ -5,9 +5,9 @@ const AVdavMotivationSchema = new Schema({
   region: { type: String, required: true },
   storeId: { type: String, required: true },
   mcsId: { type: String, required: true },
-  soar: { type: Number, required: true },
-  soqty: { type: Number, required: true },
-  ttlar: { type: Number, required: true },
+  soar: { type: String, required: true },
+  soqty: { type: String, required: true },
+  ttlar: { type: String, required: true },
   rank: { type: Number, required: true },
 });
 
