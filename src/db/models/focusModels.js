31d92caa95a0a4df5afc32b943c484cd4e53742f
Sergey Shulga <shul.sergio@gmail.com>
2026-06-15 15:20:31 +0300

    Update focusModels.js

--- src/db/models/focusModels.js
+++ src/db/models/focusModels.js
@@ -1,19 +1,15 @@
 import { model, Schema } from 'mongoose';
 
-const focusModelsDataSchema = new Schema({
-  year: { type: Number, required: true },
-  month: { type: String, required: true },
-  sku: { type: String, required: true },
-  prd: { type: String, required: true },
-  type: { type: String, required: true },
-  rrp: { type: String, required: true },
-  focus: { type: Number, required: true },
-  topFocus: { type: Number, required: true },
-});
-
 const focusModelsSchema = new Schema(
   {
-    data: [focusModelsDataSchema],
+    year: { type: Number, required: true },
+    month: { type: String, required: true },
+    sku: { type: String, required: true },
+    prd: { type: String, required: true },
+    type: { type: String, required: true },
+    rrp: { type: String, required: true },
+    focus: { type: Number, required: true },
+    topFocus: { type: Number, required: true },
   },
   { timestamps: true, versionKey: false },
 );
